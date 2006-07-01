16eae08586ba581bb212b21a625d43e3e1198526
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2006-07-01 01:10:08 +0000

    [2006-07-01]Initial content.

---
+++ common-include/trunk/EnsureClnup.h
@@ -0,0 +1,148 @@
+#ifndef __ENSURECLNUP_H
+#define __ENSURECLNUP_H
+
+/* The idea comes from Jeffrey Richter's CEnsureCleanup template class, 
+ which is presented in his book 
+   << Programming Server-Side Applications for Microsoft Windows 2000 >>
+
+	But I found that his original CEnsureCleanup is not acceptable when
+ compiling with GCC 3.2, and with the compiler from Borland C++ 5.5 .
+
+	So I did the improvement.
+*/
+
+
+template<typename PTR_TYPE, typename RET_TYPE, RET_TYPE (*pfn)(PTR_TYPE)> 
+class CEnsureCleanupPtr
+{
+	PTR_TYPE m_t;           // The member representing the object
+
+public:
+	// Default constructor assumes an invalid value (nothing to cleanup)
+	CEnsureCleanupPtr() { m_t = NULL; }
+	
+	// This constructor sets the value to the specified value
+	CEnsureCleanupPtr(PTR_TYPE t) : m_t(t) { }
+	
+	// The destructor performs the cleanup.
+	~CEnsureCleanupPtr() { Cleanup(); }
+	
+	// Helper methods to tell if the value represents a valid object or not..
+	int IsValid() { return(m_t != NULL); }
+	int IsInvalid() { return(!IsValid()); }
+	
+	// Re-assigning the object forces the current object to be cleaned-up.
+	PTR_TYPE operator=(PTR_TYPE t) 
+	{ 
+		Cleanup(); 
+		m_t = t;
+		return(*this);  
+	}
+	
+	// Returns the value (supports both 32-bit and 64-bit Windows).
+	operator PTR_TYPE() 
+	{ 
+		return m_t;
+	}
+	
+	// Cleanup the object if the value represents a valid object
+	void Cleanup() 
+	{ 
+		if (IsValid()) 
+		{
+			pfn(m_t);         // Cleanup the object.
+			m_t = NULL;   // We no longer represent a valid object.
+		}
+	}
+};
+
+
+template<class INT_TYPE, class RET_TYPE, RET_TYPE (*pfn)(INT_TYPE), INT_TYPE tInvalid> 
+class CEnsureCleanupInt
+{
+	INT_TYPE m_t;           // The member representing the object
+
+public:
+	// Default constructor assumes an invalid value (nothing to cleanup)
+	CEnsureCleanupInt() { m_t = tInvalid; }
+	
+	// This constructor sets the value to the specified value
+	CEnsureCleanupInt(INT_TYPE t) : m_t(t) { }
+	
+	// The destructor performs the cleanup.
+	~CEnsureCleanupInt() { Cleanup(); }
+	
+	// Helper methods to tell if the value represents a valid object or not..
+	int IsValid() { return(m_t != tInvalid); }
+	int IsInvalid() { return(!IsValid()); }
+	
+	// Re-assigning the object forces the current object to be cleaned-up.
+	INT_TYPE operator=(INT_TYPE t) 
+	{ 
+		Cleanup(); 
+		m_t = t;
+		return(*this);  
+	}
+	
+	// Returns the value (supports both 32-bit and 64-bit Windows).
+	operator INT_TYPE() 
+	{ 
+		return m_t;
+	}
+	
+	// Cleanup the object if the value represents a valid object
+	void Cleanup() 
+	{ 
+		if (IsValid()) 
+		{
+			pfn(m_t);         // Cleanup the object.
+			m_t = tInvalid;   // We no longer represent a valid object.
+		}
+	}
+	
+};
+
+
+#define MakeCleanupPtrClass(ClassName, RET_TYPE_of_CleanupFunction, pCleanupFunction, PTR_TYPE) \
+	typedef CEnsureCleanupPtr<PTR_TYPE, RET_TYPE_of_CleanupFunction, pCleanupFunction> ClassName;
+
+#define MakeCleanupIntClass(ClassName, RET_TYPE_of_CleanupFunction, pCleanupFunction, INT_TYPE, IntValueInvalid) \
+	typedef CEnsureCleanupInt<INT_TYPE, RET_TYPE_of_CleanupFunction, pCleanupFunction, IntValueInvalid> ClassName;
+
+
+#define MakeCleanupPtrClass_delete(ClassName, PTR_TYPE) \
+	inline void __delete_##ClassName(PTR_TYPE p) { delete p; } \
+	MakeCleanupPtrClass(ClassName, void, __delete_##ClassName, PTR_TYPE)
+
+/* Some usage examples:
+
+MakeCleanupIntClass(Cec_LibcFh, int, close, int, -1)
+	// Use ` int close(int); ' to cleanup a file handle opened by 
+	// ` int open (const char *, int, ...); ' , and the invalid(default) value is -1 .
+
+
+MakeCleanupPtrClass(Cec_pVoid, void, free, void*)
+	// Use ` void free(void*); ' to cleanup a buffer allocated by 
+	// ` void* malloc(size_t); ' .
+
+
+inline void free_unsigned_char_ptr(unsigned char * p){ free(p); }
+
+MakeCleanupPtrClass(Cec_pUchar, void, free_unsigned_char_ptr, unsigned char *)
+	// Use ` void free_unsigned_char_ptr(unsigned char * p); ' to cleanup a buffer 
+	// allocated by ` void* malloc(size_t); ' . For the sake of function pointer
+	// type matching, you have to define a wrapper function for free() .
+
+int main()
+{
+	Cec_LibcFh fh = open("_TestFile.txt", O_CREAT|O_RDWR|O_TRUNC ,_S_IWRITE);
+
+	Cec_pVoid pv = malloc(100);
+
+	Cec_pUchar puc = (unsigned char*)malloc(200);
+
+	return 0;
+}
+*/
+
+#endif
---
+++ common-include/trunk/armcpp/ps_TCHAR.h
@@ -0,0 +1,47 @@
+#ifndef __ps_TCHAR_h_for_armcpp_20060109_
+#define __ps_TCHAR_h_for_armcpp_20060109_
+
+// IMPORTANT: See mswin/ps_TCHAR.h for important comment of using this header.
+
+#ifdef _UNICODE		/* ================ _UNICODE ================ */
+
+#ifndef __T
+ #define __T(x)	L##x
+#endif
+
+#ifndef _TCHAR_DEFINED
+ typedef wchar_t     TCHAR;
+ #define _TCHAR_DEFINED
+#endif
+
+
+
+#else	/* ================ Not _UNICODE ================ */
+
+
+
+#ifndef __T
+# define __T(x)	(x)
+#endif
+
+#ifndef _TCHAR_DEFINED
+ typedef char     TCHAR;
+ #define _TCHAR_DEFINED
+#endif
+
+
+#endif // #ifdef _UNICODE .else
+
+
+
+/* Generic text macros to be used with string literals and character constants.
+   Will also allow symbolic constants that resolve to same. */
+
+#define _T(x)       __T(x)
+#define _TEXT(x)    __T(x)
+
+
+
+
+
+#endif
