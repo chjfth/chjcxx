b78af0176924b0bd1e777a37290febd1077562ec
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2008-08-27 05:28:14 +0000

    temp

--- gadgetlib/trunk/libsrc/include/gadgetlib/dirfuncs.h
+++ gadgetlib/trunk/libsrc/include/gadgetlib/dirfuncs.h
@@ -24,16 +24,31 @@ bool ggt_FreeDirfuncBuffer(TCHAR *p);
 	//!< Free dir or path buffer allocated by functions below.
 
 
-TCHAR * ggt_RelPathToAbsPath(const TCHAR *rel);
+TCHAR * ggt_RelPathToAbsPath(const TCHAR *rel, TCHAR *bufprev);
 	//!< Convert relative path to absolute path.
 	/*!< This is only literal operation, not checking disk files.
 
 	@param[in] rel
 		The relative path. This can be an absolute path as well.
 
+	@param[in] bufprev
+		Buffer pointer returned by previous ggt_RelPathToAbsPath() or other functions
+		declared here. 
+		- If NULL, a new buffer will be allocated for the caller.
+		- If non-NULL, this function will check whether bufprev can be re-used to accommodate
+		  the result, this improves some performance. The internal works like ANSI C realloc().
+
 	@return
-		Return a string pointer to the resulting absolute path. (can be null?)
+		Return a string pointer to the resulting absolute path.
 		The returned buffer should be freed by ggt_FreeDirfuncBuffer();
+
+		- If return value is NULL, it means path buffer allocation fail and bufprev remains
+		  intact. You should later free bufprev .
+		- If return value is non-NULL, the input bufprev variable can be discarded; you 
+		  just get the result from the return value(may or may not be the same as bufprev)
+		  and free it later.
+		  
+
 	*/
 
 
---
+++ gadgetlib/trunk/libsrc/include/gadgetlib/fstream-aux.h
@@ -0,0 +1,31 @@
+#ifndef __fstream_aux_h_20080827_
+#define __fstream_aux_h_20080827_
+
+#ifdef __cplusplus
+extern"C" {
+#else
+#define bool int; // sly
+#endif
+
+
+#include <stdio.h>
+
+// struct FILE; // cause VC6 compile error if <stdio.h> is included later : 
+//	error C2371: 'FILE' : redefinition; different basic types
+// VC6 CRT used: 
+//	typedef struct _iobuf FILE;
+
+bool ggt_fstream_change_file_size(FILE *fp, int newsize);
+	// [2008-08-27]
+	// Since the ANSI(C99) file stream operation has not standardized functions
+	// supporting 64-bit file length(e.g. no fseek64 for FILE*) , I just use 
+	// int instead of __int64.
+
+#ifdef __cplusplus
+} // extern"C" {
+#endif
+
+
+
+
+#endif
