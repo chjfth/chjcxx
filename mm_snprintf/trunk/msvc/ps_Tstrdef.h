58aa01ecd0252bd0740d1ceec2c3eb9fd82517cc
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2008-09-03 06:37:08 +0000

    Update makefile for WinCE. Reject old gmp_PS_INCLUDE_SUBDIRS.

--- mm_snprintf/trunk/msvc/ps_Tstrdef.h
+++ mm_snprintf/trunk/msvc/ps_Tstrdef.h
@@ -5,7 +5,13 @@
 
 #define TMM_strchr _tcschr
 #define TMM_strlen _tcslen
-#define TMM_isdigit _istdigit
+
+#ifndef WINCE
+# define TMM_isdigit _istdigit
+#else
+# define TMM_isdigit iswdigit // Just always use the wide-char variant on WinCE
+#endif
+	// Damn M$, hide _istdigit for WinCE app. See NLscan Wiki pageId=17432594 .
 
 #define TMM_sprintf _stprintf
 
