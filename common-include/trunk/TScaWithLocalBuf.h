5093ed04044ec60d0a8251edb3bdbea75264d5e0
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2008-09-06 13:01:21 +0000

    Add Tscawlb_ReqOptBuf, to test.

--- common-include/trunk/TScaWithLocalBuf.h
+++ common-include/trunk/TScaWithLocalBuf.h
@@ -4,7 +4,8 @@
 #include <TScalableArray.h>
 
 //! Pick an optimized buffer between a local buffer or an TScalableArray maintained buffer.
-/*!
+/*! 
+
  @param[in] LocalBuf
 	Local buffer is usually a buffer(array) allocated on a function's stack.
 
@@ -19,13 +20,18 @@
 
  @return
   This function compares two buffers provided by LocalBuf and sca, returning the
-  larger one's pointer to the caller.
+  larger one's pointer to the caller. 
+
+  NOTE: "Buffer size" are all described in T elements.
 
 */
 template<typename T>
 inline T*
-Tscawlb_PickOptBuf(T *LocalBuf, int LocalBufsize, TScalableArray<T> &sca, int *pRetSize)
+Tscawlb_PickOptBuf(T *LocalBuf, int LocalBufsize, TScalableArray<T> &sca, int *pRetSize=0)
 {
+	// Hmm, I hope I can declare `sca' const, since this function does not change sca at all.
+	// But I can't, because GetElePtr() does not return a const pointer.
+
 	int scaSize = sca.CurrentEles();
 
 	if(LocalBufsize>=scaSize)
@@ -38,6 +44,68 @@ Tscawlb_PickOptBuf(T *LocalBuf, int LocalBufsize, TScalableArray<T> &sca, int *p
 	{
 		if(pRetSize)
 			*pRetSize = scaSize;
+		return sca.GetElePtr(0);
+	}
+}
+
+//! Request an optimized buffer between a local buffer and an TScalableArray maintained buffer.
+/*! If the local buffer size is enough, local buffer pointer is returned, otherwise the
+ TScalableArray's buffer is increased to make a buffer of requested size.
+
+ @param[in] LocalBuf
+	Local buffer is usually a buffer(array) allocated on a function's stack.
+
+ @param[in] LocalBufsize
+	Size of the local buffer, in T elements.
+
+ @sca[in]
+	An TScalableArray object who can provide scalable buffer.
+
+ @pReize[in/out] (requested and returned size)
+	On input, tells the buffer size request; on output, tells the available buffer size
+	by the returned pointer. This may be larger than the requested size.
+	This output tells the size of buffer pointed to by the returned pointer.
+
+ @isShrinkSca[in]
+	If requested size is larger than LocalBuf but smaller than sca, whether call 
+	sca.SetEleQuan() so that sca's internal buffer may be shrinked. 
+
+
+ @return
+  This function compares two buffers provided by LocalBuf and sca, returning the
+  larger one's pointer to the caller.
+
+  NOTE: "Buffer size" are all described in T elements.
+
+*/
+template<typename T>
+inline T*
+Tscawlb_ReqOptBuf(T *LocalBuf, int LocalBufsize, TScalableArray<T> &sca, int *pReSize,
+	bool isShrinkSca=false)
+{
+	int scaSize = sca.CurrentEles();
+
+	if(LocalBufsize>=*pReSize)
+	{
+		*pReSize = LocalBufsize;
+		return LocalBuf;
+	}
+	else if(scaSize>=*pReSize)
+	{
+		if(isShrinkSca)
+		{
+			sca.SetEleQuan(UP_ROUND(*pReSize, sca.GetIncSize()), NO);
+			*pReSize = sca.CurrentEles();
+		}
+		else
+			*pReSize = scaSize;
+
+		return sca.GetElePtr(0);
+	}
+	else // must increase sca buffer
+	{
+		sca.SetEleQuan(UP_ROUND(*pReSize, sca.GetIncSize()), NO);
+		*pReSize = sca.CurrentEles();
 		return sca.GetElePtr(0);
 	}
 }
