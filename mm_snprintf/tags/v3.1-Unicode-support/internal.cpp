573ba99109b5da1a0eb9d01e79800a303815706a
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2008-11-25 05:11:35 +0000

    This tag, can be compiled for WinCE. Actually, I support Unicode and non-Unicode functions in one library, -- parallel .obj/.o .
    If user don't want Unicode code, just set MMSNPRINTF_NO_UNICODE to non-null.

---
+++ mm_snprintf/tags/v3.1-Unicode-support/internal.cpp
@@ -0,0 +1,10 @@
+#include "mm_snprintf.h"
+#include "internal.h"
+
+unsigned short
+mmsnprintf_getversion(void)
+{
+	return (PORTABLE_SNPRINTF_VERSION_MAJOR<<8) | PORTABLE_SNPRINTF_VERSION_MINOR;
+}
+
+
