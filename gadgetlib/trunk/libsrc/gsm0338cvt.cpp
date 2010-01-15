1791358afcf0746fbdd93b363b9faa2289b934fa
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2010-01-15 00:52:49 +0000

    Fix! Assign '\xA5' to wchar_t will result in a value of 0xFFA5, -- that's not desired, so use 0xA5 now.

--- gadgetlib/trunk/libsrc/gsm0338cvt.cpp
+++ gadgetlib/trunk/libsrc/gsm0338cvt.cpp
@@ -3,6 +3,9 @@
 //wchar_t ww = L'\xA3';
 	//[2010-01-12] This will cause VC6 SP6 compiler spout error:
 	//	error C2002: invalid wide-character constant
+	//It seems the L'\x' represented char value must not be 0x81-0xFF,
+	//so, I'would use 0xA3 instead of L'\xA3' as a workaround.
+	//VS2005 does not have such problem.
 
 struct map_gsmc2uc_st
 {
@@ -13,56 +16,57 @@ struct map_gsmc2uc_st
 map_gsmc2uc_st g_map_gsmc2uc[] =
 {
 	0,   '@',
-	1,   '\xA3', // British pound sign
+	1,   0xA3, // British pound sign
 	2,   '$',
-	3,   '\xA5', // Chinese Yuan
-	4,   '\xE8', // e with grave
-	5,   '\xE9', // e with acute
-	6,   '\xF9', // u with grave
-	7,   '\xEC', // i with grave
-	8,   '\xF2', // o with grave
-	9,   '\xC7', // C with cedilla
+	3,   0xA5, // Chinese Yuan
+	4,   0xE8, // e with grave
+	5,   0xE9, // e with acute
+	6,   0xF9, // u with grave
+	7,   0xEC, // i with grave
+	8,   0xF2, // o with grave
+	9,   0xC7, // C with cedilla
 	0xA, '\n', // new line
-	0xB, '\xD8', // O slash 
-	0xC, '\xF8', // o slash 
+	0xB, 0xD8, // O slash 
+	0xC, 0xF8, // o slash 
 	0xD, '\r', // carriage return 
-	0xE, '\xC5', // A ring above
-	0xF, '\xE5', // a ring above
-	0x10,L'\x0394', // delta
+	0xE, 0xC5, // A ring above
+	0xF, 0xE5, // a ring above
+	0x10,0x0394, // delta
 	0x11,'_', //
-	0x12,L'\x03A6', // phi (greek)
-	0x13,L'\x0393', // gama
-	0x14,L'\x039B', // lamda
-	0x15,L'\x03A9', // omega
-	0x16,L'\x03A0', // pi
-	0x17,L'\x03A8', // psi
-	0x18,L'\x03A3', // sigma
-	0x19,L'\x0398', // theta
-	0x1A,L'\x039E', // xi
-	0x1B,'\x1B', // ( the escape symbol )
-	0x1C,'\xC6', // AE capital (latin)
-	0x1D,'\xE6', // ae small
-	0x1E,'\xDF', // beta
-	0x1F,'\xC9', // E with acute
+	0x12,0x03A6, // phi (greek)
+	0x13,0x0393, // gama
+	0x14,0x039B, // lamda
+	0x15,0x03A9, // omega
+	0x16,0x03A0, // pi
+	0x17,0x03A8, // psi
+	0x18,0x03A3, // sigma
+	0x19,0x0398, // theta
+	0x1A,0x039E, // xi
+	0x1B,0x1B, // ( the escape symbol )
+	0x1C,0xC6, // AE capital (latin)
+	0x1D,0xE6, // ae small
+	0x1E,0xDF, // beta
+	0x1F,0xC9, // E with acute
 
-	0x24, '$', // 0x24 is currency sign, I use $ here for simplicity
+//	'$', 0xA4, // 0x24($) maps to the generic currency symbol, 
+		// If user wants to process GSM0338 0x24 more explicitly, he should do so himself.
 
-	0x40, '\xA1', // upside-down !
-	0x60, '\xBF', // upside-down ?
+	0x40, 0xA1, // upside-down !
+	0x60, 0xBF, // upside-down ?
 	
-	0x5B, '\xC4', // A with diaeresis
-	0x5C, '\xD6', // O with diaeresis
-	0x5D, '\xD1', // N with tilde
-	0x5E, '\xDC', // U with diaeresis
-	0x5F, '\xA7', // section sign
+	0x5B, 0xC4, // A with diaeresis
+	0x5C, 0xD6, // O with diaeresis
+	0x5D, 0xD1, // N with tilde
+	0x5E, 0xDC, // U with diaeresis
+	0x5F, 0xA7, // section sign
 
-	0x7B, '\xE4', // a with diaeresis
-	0x7C, '\xF6', // o with diaeresis
-	0x7D, '\xF1', // n with tilde
-	0x7E, '\xFC', // u with diaeresis
-	0x7F, '\xE0', // a with grave
+	0x7B, 0xE4, // a with diaeresis
+	0x7C, 0xF6, // o with diaeresis
+	0x7D, 0xF1, // n with tilde
+	0x7E, 0xFC, // u with diaeresis
+	0x7F, 0xE0, // a with grave
 
-	0x1B0A, '\x0C', // form feed
+	0x1B0A, 0x0C, // form feed
 	0x1B14, '^',
 	0x1B28, '{',
 	0x1B29, '}',
@@ -71,7 +75,7 @@ map_gsmc2uc_st g_map_gsmc2uc[] =
 	0x1B3D, '~',
 	0x1B3E, ']',
 	0x1B40, '|',
-	0x1B65, L'\x20AC', // Euro
+	0x1B65, 0x20AC, // Euro
 };
 
 const int gn_map_gsmc2uc = sizeof(g_map_gsmc2uc)/sizeof(g_map_gsmc2uc[0]);
@@ -79,10 +83,11 @@ const int gn_map_gsmc2uc = sizeof(g_map_gsmc2uc)/sizeof(g_map_gsmc2uc[0]);
 static bool
 is_gsm_uc_equal(int c)
 {
-	if(c>=' ' && c<='?') // 0x20~0x3F, including '0'~'9'
+	if(c>=' ' && c<='?') // 0x20~0x3F, including '0'~'9','$'
 		return true;
 	if(c>='A' && c<='Z' || c>='a' && c<='z')
 		return true;
+
 	return false;
 }
 
@@ -106,6 +111,9 @@ int
 ggt_uc2gsm(wchar_t uc)
 {
 	int i;
+	if(uc=='$' || uc==0xA4) // check this before is_gsm_uc_equal() to override '$'s mapping
+		return 0x02;
+
 	if(is_gsm_uc_equal(uc))
 		return uc;
 	
