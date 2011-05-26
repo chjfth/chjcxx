7f627bf51cf0c54b754ea0a1eb969db1814f03e9
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2011-05-26 04:41:35 +0000

    linux fix

--- gadgetlib/dev/make-sdk/sdk-linuxgcc-all/make-sdk-all.sh
+++ gadgetlib/dev/make-sdk/sdk-linuxgcc-all/make-sdk-all.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/sh
 
 curdir=`pwd`
-# set gmi_SYDO_SHOW_COPY_CMD=1
-
-export gmb_planets=all
-export gmb_planets_linuxgcc=allac
+#export gmi_SYDO_SHOW_COPY_CMD=1
 
 if [ "$gmb_syncto" == "" ]; then
 	echo  "gmb_syncto=$curdir/nlssdk"
 	export gmb_syncto=$curdir/nlssdk
 fi
 
-umake gmb_compiler_ids="all" gmb_linuxgcc_vers="all" "$@"
+umake gmb_compiler_ids="" gmb_linuxgcc_vers="" "$@"
