c17580494179407db2d00bde745caf42f2a8b34e
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2016-03-06 04:54:05 +0000

    temp, working

--- mm_snprintf/trunk/make-sdk/sdk-msvc-all/make-sdk-all.bat
+++ mm_snprintf/trunk/make-sdk/sdk-msvc-all/make-sdk-all.bat
@@ -1,36 +1,19 @@
 @echo off
 setlocal
-call %DIR_NLS_BUILD_ENV%\gmp-cenv-all.bat
 
-set batdir=%~dp0%
-set batdir=%batdir:~0,-1%
-set batdir_fs=%batdir:\=/%
+REM [optional] gmb_syncto=...
+REM -- will receive default in scalacon-make-sdk.bat
+REM set gmb_syncto=%CD%/sdkout
 
-REM set gmi_SYDO_SHOW_COPY_CMD=1
+REM set gmb_compiler_ids=msvc
+REM set gmb_msvc_vers=vc100
+REM set gmb_wince_vers=
 
-IF "%gmb_syncto%" == "" (
-	echo SET gmb_syncto=%batdir_fs%/sdkout
-         SET gmb_syncto=%batdir_fs%/sdkout
-  )
+REM [optional] gmu_SC_CHECKOUT_DATETIME=...
+REM -- will receive default in scalacon-make-sdk.bat
 
-set datecmd=date_ +"%%Y-%%m-%%d %%H:%%M:%%S"
-if "%gmu_SC_CHECKOUT_DATETIME%" == "" (
-	REM Note: Place less CMD commands here, so to avoid delayed-expansion format !foobar! .
-	for /F "usebackq delims=" %%i IN (`%datecmd%`) DO set gmu_SC_CHECKOUT_DATETIME=%%i
-)
-if "%gmu_SC_CHECKOUT_DATETIME%" == "" (
-	echo Get current datetime fail! You probably do not have date_.exe in your PATH. Install GMU to get this file.
-	exit /b 1
-)
+REM [optional] CMD_GETSDKIN
+REM -- used by scalacon-make-sdk.bat, will be NULL if not set.
+set CMD_GETSDKIN=scalacon-get-sdkin.py --ini=../../get-sdkin.ini --force
 
-REM echo gmu_SC_CHECKOUT_DATETIME=%gmu_SC_CHECKOUT_DATETIME% (debug)
-
-..\..\get-sdkin.py --force --datetime="%gmu_SC_CHECKOUT_DATETIME%"
-if ERRORLEVEL 1 (
-	echo Error: get-sdkin.py execution failed! Cannot continue.
-	exit /b 1
-)
-
-@echo on
-umaketime %*
-@REM %* may have: gmb_compiler_ids="..." gmb_msvc_vers="..." gmb_wince_vers="..." 
+scalacon-make-sdk %*
