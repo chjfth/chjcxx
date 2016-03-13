240c916466db822ff94d399a27a81c0564fe75dd
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2016-03-13 12:07:51 +0000

    update for new

--- mm_snprintf/trunk/make-sdk/sdk-msvc-all/make-sdk-all.bat
+++ mm_snprintf/trunk/make-sdk/sdk-msvc-all/make-sdk-all.bat
@@ -5,9 +5,17 @@ set gmu_DO_SHOW_VERBOSE=1
 set gmu_DO_SHOW_COMPILE_CMD=1
 set gmu_DO_SHOW_LINK_CMD=1
 
-REM gmb_syncto=... (set SDK output dir)
-REM -- will receive default in scalacon-make-sdk.bat
-set gmb_syncto=../../sdkout
+REM gmb_buildsdk_ini=... (set the INI file used by PI_makesdk_2016)
+REM -- will receive default "buildsdk.ini" in Makefile.umk
+rem set gmb_buildsdk_ini=buildsdk.ini
+
+REM gmb_dirname_sdkin=... (set dirname to receive input SDKs)
+REM -- default is null. Set it only if you use input SDKs.
+set gmb_dirname_sdkin=sdkin
+
+REM gmb_dirname_sdkout=... (set this SDK output dirname)
+REM -- will receive default "sdkout" in PI_makesdk_2016
+set gmb_dirname_sdkout=sdkout
 
 REM set gmb_compiler_ids=msvc
 REM set gmb_msvc_vers=vc100
@@ -15,6 +23,10 @@ REM set gmb_wince_vers=
 
 REM [optional] gmu_SC_CHECKOUT_DATETIME=...
 REM -- will receive default in scalacon-make-sdk.bat
+
+REM [optional] gmb_run_example_on_compiler_vers
+REM -- set this if you need to verify-run some example exe output
+REM set gmb_run_example_on_compiler_vers="vc80 vc100x64"
 
 REM [optional] CMD_GETSDKIN
 REM -- used by scalacon-make-sdk.bat, will be NULL if not set.
