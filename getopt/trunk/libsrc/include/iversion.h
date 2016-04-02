3b87a56003535c0f9c3bb6393cd8545b169e2a32
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2016-04-02 14:31:47 +0000

    minor update

--- getopt/trunk/libsrc/include/iversion.h
+++ getopt/trunk/libsrc/include/iversion.h
@@ -2,11 +2,10 @@
 #define __sgetopt_iversion_h_20160325_
 
 enum {
-	sgetopt_vmajor = 0,
-	sgetopt_vminor = 1,
+	sgetopt_vmajor = 1,
+	sgetopt_vminor = 0,
 	sgetopt_vbuild = 0,
-
-//	sgetopt_nlsr_version = 0x02
+	sgetopt_vextra = 2
 };
 
 
