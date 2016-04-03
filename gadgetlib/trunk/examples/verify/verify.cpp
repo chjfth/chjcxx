4863115e850b3fe8bec04759339328ed444c7189
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2016-04-03 05:27:58 +0000

    Add .h-PDB-sewing test code.

--- gadgetlib/trunk/examples/verify/verify.cpp
+++ gadgetlib/trunk/examples/verify/verify.cpp
@@ -3,12 +3,22 @@
 
 #include "share.h"
 
+int casual_test()
+{
+	TScalableArray<int> sa;
+	sa.SetEleQuan(10, true); // no use, just trying my .h-PDB-sewing
+	return sa[1];
+}
+
+
 #ifdef _MSC_VER
 int _tmain()
 #else
 int main()
 #endif
 {
+	casual_test();
+	
 	verify_timefuncs();
 	
 	printf("Verify success.\n");
