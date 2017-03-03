478d60bc81393b83f91e9d8cbb3d9b5402452888
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2017-03-03 05:49:36 +0000

    MakeCleanupPtrClass_winapi(Cec_HKEY, LONG, RegCloseKey, HKEY)

--- common-include/trunk/include/EnsureClnup_mswin.h
+++ common-include/trunk/include/EnsureClnup_mswin.h
@@ -7,13 +7,17 @@
 #include <EnsureClnup.h>
 
 
-MakeCleanupPtrClass_winapi(Cec_PTRHANDLE, BOOL, CloseHandle, HANDLE);
+MakeCleanupPtrClass_winapi(Cec_PTRHANDLE, BOOL, CloseHandle, HANDLE)
 	// This is for CreateEvent,OpenProcess, who return NULL as fail.
-MakeCleanupIntClass_winapi(Cec_FILEHANDLE, BOOL, CloseHandle, HANDLE, INVALID_HANDLE_VALUE); 
+MakeCleanupIntClass_winapi(Cec_FILEHANDLE, BOOL, CloseHandle, HANDLE, INVALID_HANDLE_VALUE)
 	// This is for CreateFile, who returns INVALID_HANDLE_VALUE as fail. Damn M$.
 
+MakeCleanupPtrClass_winapi(Cec_HKEY, LONG, RegCloseKey, HKEY)
+	// for RegOpenKeyEx 
+
+
 #ifdef _INC_SETUPAPI
-MakeCleanupIntClass_winapi(Cec_HDEVINFO, BOOL, SetupDiDestroyDeviceInfoList, HDEVINFO, INVALID_HANDLE_VALUE); 
+MakeCleanupIntClass_winapi(Cec_HDEVINFO, BOOL, SetupDiDestroyDeviceInfoList, HDEVINFO, INVALID_HANDLE_VALUE)
 #endif
 
 //MakeCleanupPtrClass_winapi(Cec_HDC_Release, int, ReleaseDC, ); // bad! ReleaseDC need 2 params.
