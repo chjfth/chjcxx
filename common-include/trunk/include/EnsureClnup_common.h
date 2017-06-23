db1dfc71140d8e22882c62f812ef3533920877f7
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2017-06-23 03:55:14 +0000

    inline void free_void_ptr(void * p){ free(p); } MakeCleanupPtrClass(Cec_free_pVoid, void, free_void_ptr, void*) 	// Use ` void free(void*); ' to cleanup a buffer allocated by ``void* malloc(size_t);``. 	// Memo: Using a *wrapper function free_void_ptr* is necessary if you use this with WDK7, 	// because WDK7 use __stdcall as x86 default calling convention(__stdcall compiler env) 	// while CRT `free` is always __cdecl.
    inline int fclose_FILE_ptr(FILE *fp){ return fclose(fp); } // wrapper for __stdcall compiler env
    MakeCleanupPtrClass(Cec_fclose, int, fclose_FILE_ptr, FILE*)

--- common-include/trunk/include/EnsureClnup_common.h
+++ common-include/trunk/include/EnsureClnup_common.h
@@ -5,16 +5,17 @@
 #include <stdlib.h>
 #include <EnsureClnup.h>
 
-MakeCleanupPtrClass(Cec_free_pVoid, void, free, void*)
-	// Use ` void free(void*); ' to cleanup a buffer allocated by 
-	// ` void* malloc(size_t); ' .
+inline void free_void_ptr(void * p){ free(p); }
+MakeCleanupPtrClass(Cec_free_pVoid, void, free_void_ptr, void*)
+	// Use ` void free(void*); ' to cleanup a buffer allocated by ``void* malloc(size_t);``.
+	// Memo: Using a wrapper function free_void_ptr is necessary if you use this with WDK7,
+	// because WDK7 use __stdcall as x86 default calling convention(__stdcall compiler env)
+	// while CRT `free` is always __cdecl.
 
 inline void free_char_ptr(char * p){ free(p); }
-
 MakeCleanupPtrClass(Cec_free_pchar, void, free_char_ptr, char*)
 
 inline void free_unsigned_char_ptr(unsigned char * p){ free(p); }
-
 MakeCleanupPtrClass(Cec_free_pUchar, void, free_unsigned_char_ptr, unsigned char *)
 	// Use ` void free_unsigned_char_ptr(unsigned char * p); ' to cleanup a buffer 
 	// allocated by ` void* malloc(size_t); ' . For the sake of function pointer
@@ -38,8 +39,8 @@ MakeCleanupPtrClass(Cec_delete_uint, void, _cpp_delete_uint_array, unsigned int*
 
 
 // For `` FILE* ''
-
-MakeCleanupPtrClass(Cec_fclose, int, fclose, FILE*)
+inline int fclose_FILE_ptr(FILE *fp){ return fclose(fp); } // wrapper for __stdcall compiler env
+MakeCleanupPtrClass(Cec_fclose, int, fclose_FILE_ptr, FILE*)
 
 
 #endif
