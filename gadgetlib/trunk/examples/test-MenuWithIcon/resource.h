1ce3b506c94933a3892b09f310b457b89be5707a
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2017-08-30 05:54:59 +0000

    VS2010 IDE friendly .rc
    #define _APS_NO_MFC 1 // VS2010 .rc properties: set MFC Mode to False.

--- gadgetlib/trunk/examples/test-MenuWithIcon/resource.h
+++ gadgetlib/trunk/examples/test-MenuWithIcon/resource.h
@@ -4,27 +4,22 @@
 //
 #define IDI_WINMAIN                     100
 #define IDD_WINMAIN                     101
-
-#define IDM_CONTEXTMENU 109
-	
-#define IDM_ABOUT                110
-
-#define IDM_INFORMATION          111
-#define IDM_NO_ICON              112
-#define IDM_NEWLAND              113
-#define IDM_ELEVATE              114
-#define IDC_TEXT_THEME_INFO      114
-#define IDB_BITMAP1              115
-
-#define IDI_NEWLAND         120
-#define IDI_VISTAICONMENU   121
-
-#define IDC_STATIC  -1
+#define IDM_CONTEXTMENU                 109
+#define IDM_ABOUT                       110
+#define IDM_INFORMATION                 111
+#define IDM_NO_ICON                     112
+#define IDM_NEWLAND                     113
+#define IDM_ELEVATE                     114
+#define IDC_TEXT_THEME_INFO             114
+#define IDB_BITMAP1                     115
+#define IDI_NEWLAND                     120
+#define IDI_VISTAICONMENU               121
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1016
