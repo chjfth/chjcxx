c18fba543f8dfd9182ed69e14cf24e9665e32d12
chj <chj@1121b4dc-be05-4708-a2e5-9da400b89831>
2017-09-05 05:28:30 +0000

    Usage comment

--- gadgetlib/trunk/libsrc/include/gadgetlib/enum_monitors.h
+++ gadgetlib/trunk/libsrc/include/gadgetlib/enum_monitors.h
@@ -15,6 +15,15 @@ struct OneMonitorInfo_st
 WinErr_t ggt_EnumMonitors(OneMonitorInfo_st arMonInfo[], int arEles, int *pTotalMonitors);
 	// Return ERROR_MORE_DATA if arEles is too small(smaller than *pTotalMonitors).
 
+/* Usage example:
+	CAutoBuf<OneMonitorInfo_st, sizeof(OneMonitorInfo_st)> abMonsinfo;
+	WinErr_t winerr = 0;
+	do {
+		winerr = ggt_EnumMonitors(abMonsinfo, abMonsinfo, abMonsinfo);
+	} while(winerr==ERROR_MORE_DATA);
+*/
+
+
 
 #ifdef __cplusplus
 } // extern"C"{
