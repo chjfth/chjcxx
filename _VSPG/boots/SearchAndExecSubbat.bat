da33e91dd02ee3d02ebdae1f90cad8afddcdfbb1
Jimm Chen WDK10 <chjfth@gmail.com>
2022-03-11 14:59:22 +0800

    Add VSPG

---
+++ _VSPG/boots/SearchAndExecSubbat.bat
@@ -0,0 +1,91 @@
+REM This bat is a function.
+
+REM setlocal EnableDelayedExpansion // Don't do this now, do it later! The Subbat may need to export arbitrary env-vars to outer env.
+REM Chj memo: It seems I will inevitably leaks some vars to the environment, _vspg_SubbatParams etc.
+
+set batfilenam=%~n0%~x0
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+
+REM Search for a series of dirs passed in as parameters, and call subbat(s) from them.
+REM Param1: "Greedy0" or "Greedy1". 
+REM 	If Greedy0, only call the first found subbat.
+REM 	If Greedy1, call each of the found subbats.
+REM Param2: Subbat filenam (without dir prefix).
+REM Param3: All params passed to subbat.
+REM         (when pass in, surrounded by quotes, when calling subbat, quotes stripped)
+REM Params remain: Each param is a directory to search for subbat.
+
+  rem call :Echos CALLED WITH: %*
+  
+  set _tmp_greedy=%~1
+  set _tmp_greedy=%_tmp_greedy:~-1%
+  REM -- _tmp_greedy will be 0 or 1 (only need final char)
+  
+  shift
+  set _vspg_SubbatFilenam=%~1
+  
+  shift
+  set _vspg_SubbatParams=%~1
+  REM    %1 example:
+  REM
+  REM    """D:\chj\AAA BBB\Chap03"" ""D:\chj\AAA BBB\Chap03\HelloWinD"" ""Debug"" "x64" ""D:\chj\AAA BBB\Chap03\x64\Debug"" ""HelloWin.exe"" ""HelloWin"" ""x64\Debug"""
+  REM
+  REM    Special Note: All directory subparam above must NOT end with a backslash, otherwise, [Shortcut1] will not work.
+  
+  set _vspg_SearchedDirs=
+  REM -- Each searched dir will be appended to the var, with minor decoration, like this:
+  REM -- [*c:\dir1*][*c:\dir2*]
+  
+:loop_SearchAndExecSubbat  
+  
+  setlocal EnableDelayedExpansion
+
+  shift
+  
+  set trydir=%~1
+  
+  if "%trydir%" == "" exit /b 0
+  
+  set trydirdeco=[*%trydir%*]
+  call "%batdir%\IsSubStr.bat" isfound "%_vspg_SearchedDirs%" "%trydirdeco%"
+  if %isfound% == 1 goto :endlocal_GoNextLoop
+
+  set _vspg_SearchedDirs=%_vspg_SearchedDirs%%trydirdeco%
+
+  set trybat=%trydir%\%_vspg_SubbatFilenam%
+
+  if not exist "%trybat%" goto :endlocal_GoNextLoop
+  
+  REM [Shortcut1] Just replace "" with " ; that is enough to pass VSproj's packed params to %trybat%.
+  endlocal & ( call "%trybat%" %_vspg_SubbatParams:""="% )
+
+  if errorlevel 1 exit /b 4
+  
+  if "%_tmp_greedy%" == "1" goto :loop_SearchAndExecSubbat
+  
+  exit /b 0
+
+:endlocal_GoNextLoop
+  endlocal
+  goto :loop_SearchAndExecSubbat
+
+
+REM =============================
+REM ====== Functions Below ======
+REM =============================
+
+REM %~n0%~x0 is batfilenam
+:Echos
+  echo [%~n0%~x0] %*
+exit /b 0
+
+:EchoExec
+  echo [%~n0%~x0] EXEC: %*
+exit /b 0
+
+:EchoVar
+  REM Env-var double expansion trick from: https://stackoverflow.com/a/1200871/151453
+  set _Varname=%1
+  for /F %%i in ('echo %_Varname%') do echo [%batfilenam%] %_Varname% = !%%i!
+exit /b 0
---
+++ _VSPG/boots/VSPG-Boots.bat
@@ -0,0 +1,76 @@
+@echo off
+setlocal EnableDelayedExpansion
+REM This bat file boots actual VSPG bat files. It should be kept minimalist. 
+REM This file exists to ease VSPG developer to temporarily switch local [boots] dir to [boots-dev].
+REM so that a new VSPG version can be developed in side-by-side with the actual project using VSPG.
+REM
+REM Operation hint: Create a soft link boots-dev pointing to VSPG original source.
+REM
+REM 	cd D:\some\big work\_VSPG
+REM 	if exists ".\boots\VSPG-Boots.bat" (echo Condition OK.)
+REM 	mklink /j boots-dev "D:\gitw\dailytools\cmd-batch\vsproj-VSPG\_VSPG\boots"
+REM 	
+REM Now, "D:\some\big work\_VSPG\boots\VSPG-Boots.bat" will next-step call content in
+REM      "D:\some\big work\_VSPG\boots-dev\VSPG-StartBat9_.bat" 
+REM instead of
+REM	     "D:\some\big work\_VSPG\boots\VSPG-StartBat9_.bat"
+REM .
+
+REM set batfilenam to .bat filename(no directory prefix)
+set batfilenam=%~n0%~x0
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+
+set _vspg_bootsdir=%~dp0
+set _vspg_bootsdir=%_vspg_bootsdir:~0,-1%
+REM
+
+call "%_vspg_bootsdir%\PathSplit.bat" "%_vspg_bootsdir%" ParentDir Subdir
+
+set "_vspg_userbatdir=%ParentDir%"
+
+
+if exist "%ParentDir%\boots-dev\VSPG-StartBat9_.bat" (
+	REM Override _vspg_bootsdir to be the -dev one.
+	set "_vspg_bootsdir=%ParentDir%\boots-dev"
+	set "_vspg_use_dev=1"
+	call :Echos Detected [boots-dev]. Will use VSPG from "!_vspg_bootsdir!" .
+)
+
+call "%_vspg_bootsdir%\VSPG-StartBat9_.bat" %*
+
+if errorlevel 1 goto :END
+
+REM ======== copy [boots-dev] to [boots] if necessary ========
+
+if "%_vspg_use_dev%" == "" goto :DONE_COPY_DEV_TO_USER
+if "%vspg_COPY_DEV_TO_USER%" == "" goto :DONE_COPY_DEV_TO_USER
+
+call :Echos Copying [boots-dev] content to user [boots] ...
+
+copy "%_vspg_bootsdir%\*.bat"   "%batdir%" 
+if errorlevel 1 exit /b 4
+copy "%_vspg_bootsdir%\*.props" "%batdir%" 
+if errorlevel 1 exit /b 4
+
+:DONE_COPY_DEV_TO_USER
+
+
+goto :END
+
+REM =============================
+REM ====== Functions Below ======
+REM =============================
+
+:Echos
+  echo [%batfilenam%] %*
+exit /b 0
+
+:EchoExec
+  echo [%batfilenam%] EXEC: %*
+exit /b 0
+
+:END
+REM [2022-01-11] Chj: We must write %ERRORLEVEL% after /b, otherwise,
+REM MSBuild(it calls us with 'cmd /c tmpXXX.bat') will always receive exit-code 0.
+exit /b %ERRORLEVEL%
