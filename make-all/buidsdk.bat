16fadbc07de65af5c5e062d7e4abd278c01d15e7
Jimm Chen WDK10 <chjfth@gmail.com>
2022-04-01 22:30:44 +0800

    Use buildsdk.bat to build all variants of libs in chjcxx.

---
+++ make-all/buidsdk.bat
@@ -0,0 +1,48 @@
+@echo off
+setlocal EnableDelayedExpansion
+set batfilenam=%~n0%~x0
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+set _vspgINDENTS=%_vspgINDENTS%.
+call :Echos START from %batdir%
+
+REM Chj memo: cmdtimeit buildsdk.bat 
+
+if not defined vsps_OUTPUT_ROOT (
+	call :SetEnvVar vsps_OUTPUT_ROOT=%batdir%\gf
+)
+
+call :Echos Using vsps_OUTPUT_ROOT=%vsps_OUTPUT_ROOT%
+
+msbuild "%batdir%\buildsdk.proj" /fl  /flp:Verbosity=normal
+
+
+exit /b 0
+
+REM =============================
+REM ====== Functions Below ======
+REM =============================
+
+:Echos
+  REM This function preserves %ERRORLEVEL% for the caller,
+  REM and, LastError does NOT pollute the caller.
+  setlocal & set LastError=%ERRORLEVEL%
+  echo %_vspgINDENTS%[%batfilenam%] %*
+exit /b %LastError%
+
+:EchoAndExec
+  echo %_vspgINDENTS%[%batfilenam%] EXEC: %*
+  call %*
+exit /b %ERRORLEVEL%
+
+:Echos0
+  REM This function preserves %ERRORLEVEL% for the caller,
+  REM and, LastError does NOT pollute the caller.
+  setlocal & set LastError=%ERRORLEVEL%
+  echo %_vspgINDENTS%.[%~n0%~x0] %*
+exit /b %LastError%
+
+:SetEnvVar
+  call :Echos0 set %*
+  set %*
+exit /b 0
