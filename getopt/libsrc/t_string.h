95e260852f8b1bebf68054580f26f28ef9cb2163
Jimm at Ub20.04-cppdev VM <chj@Ub2.04-cppdev>
2024-01-19 11:18:41 +0800

    Use C style internal names for t_string.h functions

--- getopt/libsrc/t_string.h
+++ getopt/libsrc/t_string.h
@@ -5,6 +5,10 @@
 
 #include "need_TCHAR.h"
 
+#ifdef __cplusplus
+extern"C" {
+#endif
+
 int t_strlen(const TCHAR *str);
 
 TCHAR * t_strchr(const TCHAR *str, TCHAR chr);
@@ -14,5 +18,9 @@ int t_strcmp(const TCHAR *str1, const TCHAR *str2);
 int t_strncmp(const TCHAR *str1, const TCHAR *str2, size_t count);
 
 int t_fprintf(FILE *stream, const TCHAR *fmt, ...);
+
+#ifdef __cplusplus
+} // extern"C" {
+#endif
 
 #endif
