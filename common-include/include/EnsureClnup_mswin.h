3a77e629a9ad98e86afd2525225e84af82de03fa
chj Win7Evernote VM <chjfth@gmail.com>
2025-03-05 10:55:15 +0800

    20250305: New naming convention: global header class names should start with CEC_ .

--- common-include/include/EnsureClnup_mswin.h
+++ common-include/include/EnsureClnup_mswin.h
@@ -1,51 +1,54 @@
-#ifndef __EnsureClnup_mswin_h_20250209_
-#define __EnsureClnup_mswin_h_20250209_
+#ifndef __EnsureClnup_mswin_h_20250305_
+#define __EnsureClnup_mswin_h_20250305_
 
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 #include "EnsureClnup.h"
 
+// [2025-03-05] Delega class naming convention: 
+// For this global header file, class name should start with CEC_ (3 capital letters).
+//
+// BTW: For a delega class defined in local project headers, it should start with Cec_ .
 
-MakeDelega_CleanupCxxPtrN(TCHAR, Cec_delete_TCHAR)
+MakeDelega_CleanupCxxPtrN(TCHAR, CEC_delete_TCHAR)
 
 
-MakeCleanupPtrClass_winapi(Cec_PTRHANDLE, BOOL, CloseHandle, HANDLE)
+MakeDelega_CleanupPtr_winapi(CEC_PTRHANDLE, BOOL, CloseHandle, HANDLE)
 	// This is for CreateEvent,OpenProcess, who return NULL as fail.
-MakeCleanupClass_winapi(Cec_FILEHANDLE, BOOL, CloseHandle, HANDLE, INVALID_HANDLE_VALUE)
+MakeDelega_CleanupAny_winapi(CEC_FILEHANDLE, BOOL, CloseHandle, HANDLE, INVALID_HANDLE_VALUE)
 	// This is for CreateFile, who returns INVALID_HANDLE_VALUE as fail. Damn M$.
 
-MakeCleanupPtrClass_winapi(Cec_HKEY, LONG, RegCloseKey, HKEY)
+MakeDelega_CleanupPtr_winapi(CEC_RegCloseKey, LONG, RegCloseKey, HKEY)
 	// for RegOpenKeyEx 
 
-MakeCleanupPtrClass_winapi(Cec_DeleteDC, BOOL, DeleteDC, HDC)
+MakeDelega_CleanupPtr_winapi(CEC_DeleteDC, BOOL, DeleteDC, HDC)
 	// for CreateCompatibleDC, CreateDC
 
-MakeCleanupPtrClass_winapi(Cec_DestroyMenu, BOOL, DestroyMenu, HMENU)
+MakeDelega_CleanupPtr_winapi(CEC_DestroyMenu, BOOL, DestroyMenu, HMENU)
 	// for CreatePopupMenu
-MakeCleanupPtrClass_winapi(Cec_DestroyIcon, BOOL, DestroyIcon, HICON)
+MakeDelega_CleanupPtr_winapi(CEC_DestroyIcon, BOOL, DestroyIcon, HICON)
 	// for CreateIconIndirect, CopyIcon, SHGetStockIconInfo 
 
-MakeCleanupPtrClass_winapi(Cec_LocalFree, HLOCAL, LocalFree, HLOCAL)
+MakeDelega_CleanupPtr_winapi(CEC_LocalFree, HLOCAL, LocalFree, HLOCAL)
 	// for ConvertSidToStringSid, ConvertStringSidToSid 
 
-MakeCleanupPtrClass_winapi(Cec_FreeSid, PVOID, FreeSid, PSID)
+MakeDelega_CleanupPtr_winapi(CEC_FreeSid, PVOID, FreeSid, PSID)
 	// for AllocateAndInitializeSid
 
 // SetupDi... functions:
 
 #ifdef _INC_SETUPAPI // #include <setupapi.h>
 
-MakeCleanupClass_winapi(Cec_HDEVINFO, BOOL, SetupDiDestroyDeviceInfoList, HDEVINFO, INVALID_HANDLE_VALUE)
+MakeDelega_CleanupAny_winapi(CEC_HDEVINFO, BOOL, SetupDiDestroyDeviceInfoList, HDEVINFO, INVALID_HANDLE_VALUE)
 
-MakeCleanupClass_winapi(Cec_SetupDiHKEY, LONG, RegCloseKey, HKEY, (HKEY)INVALID_HANDLE_VALUE)
+MakeDelega_CleanupAny_winapi(CEC_SetupDiHKEY, LONG, RegCloseKey, HKEY, (HKEY)INVALID_HANDLE_VALUE)
 	// for SetupDiOpenDevRegKey
 	// Note: We need to write '(HKEY)INVALID_HANDLE_VALUE', otherwise we get VS2010 cl.exe error:
 	// error C2440: 'specialization' : cannot convert from 'HANDLE' to 'HKEY'
 
 #endif
 
-//MakeCleanupPtrClass_winapi(Cec_HDC_Release, int, ReleaseDC, ); // bad! ReleaseDC need 2 params.
+//MakeDelega_CleanupPtr_winapi(CEC_HDC_Release, int, ReleaseDC, ); // Bad! ReleaseDC needs 2 params.
 
 
-#endif
+#endif // __EnsureClnup_mswin_h_
