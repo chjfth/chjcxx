266c229b3ca75905bdbcd8de963dac89ef5772f4
chj Win7Evernote VM <chjfth@gmail.com>
2025-04-03 10:57:17 +0800

    new file VC2010_future.h

---
+++ common-include/include/mswin/VC2010_future.h
@@ -0,0 +1,22 @@
+#ifndef __VC2010_future_h_20250403_
+#define __VC2010_future_h_20250403_
+
+// This file supplies macros, enum, functions introduced after VC2010/Win7 SDK,
+// so that VC2010 vcxproj can use future symbols.
+//
+// Be aware, please protect future symbols with proper _WIN32_WINNT version check,
+// so that this file is compatible with real future Visual C++/SDK releases.
+//
+// Sure, this file should be included AFTER all system headers for best compatibility.
+
+
+#if _WIN32_WINNT < 0x0A00
+// enum SID_NAME_USE has more values.
+enum { SidTypeLogonSession = 11 };
+#endif
+
+
+
+
+#endif
+
