b36805be465afddc792f3a326e394b721bbd1df7
chj Win7Evernote VM <chjfth@gmail.com>
2025-04-11 16:33:33 +0800

    Update ChunkHelper to be a C++ template.

--- common-include/include/ChunkHelper.h
+++ common-include/include/ChunkHelper.h
@@ -1,35 +1,51 @@
-#ifndef __ChunkHelper_h_20250410_
-#define __ChunkHelper_h_20250410_
+#ifndef __ChunkHelper_h_20250411_
+#define __ChunkHelper_h_20250411_
 
 #include <assert.h>
 
-class CChunkHelper
+template<typename T>
+class TChunkHelper
 {
 public:
-	CChunkHelper(int totalsize, int chunksize) :
-		m_totalsize(totalsize), m_chunksize(chunksize), m_remain(totalsize)
-	{}
+	TChunkHelper(T totalsize, T chunksize)
+	{
+		reset(totalsize, chunksize);
+	}
 
-	int next() 
+	void reset(T totalsize, T chunksize)
+	{
+		m_totalsize = totalsize;
+		m_chunksize = chunksize;
+		m_offset = 0;  m_remain = totalsize;
+	}
+
+	T next() const
 	{
 		// pick min(chuck, remain)
 		return m_chunksize < m_remain ? m_chunksize : m_remain;
 	}
 	
-	int drop(int dropsize) 
+	T drop(T dropsize) 
 	{
 		assert(dropsize >= 0);
 		assert(dropsize <= m_remain);
 
+		m_offset += dropsize;
 		m_remain -= dropsize;
 		return m_remain;
 	}
 
+	T offset()
+	{
+		return m_offset;
+	}
+
 
 private:
-	int m_totalsize;
-	int m_chunksize;
-	int m_remain;
+	T m_totalsize;
+	T m_chunksize;
+	T m_remain;
+	T m_offset;
 };
 
 
