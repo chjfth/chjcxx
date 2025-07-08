1f1448fba554d7159097623fa0d693a95ac4bcc9
chj Win7Evernote VM <chjfth@gmail.com>
2025-07-08 22:44:21 +0800

    Combobox_EnableWideDrop now uses CxxWindowSubclass lib.

--- common-include/include/mswin/Combobox_EnableWideDrop.h
+++ common-include/include/mswin/Combobox_EnableWideDrop.h
@@ -1,5 +1,5 @@
-#ifndef __Combobox_EnableWideDrop_h_20250326_
-#define __Combobox_EnableWideDrop_h_20250326_
+#ifndef __Combobox_EnableWideDrop_h_20250708_
+#define __Combobox_EnableWideDrop_h_20250708_
 
 #include <windows.h>
 
@@ -11,7 +11,7 @@ enum DlgboxCbw_err
 	// Error on enable
 	DlgboxCbw_NoMem = 2,
 	DlgboxCbw_AlreadyEnabled = 3,
-	DlgboxCbw_SetProp = 4, // WinAPI SetProp() error
+	DlgboxCbw_BadParam = 4, 
 
 	// Error on disable
 	DlgboxCbw_NotEnabledYet = 10,
@@ -35,46 +35,47 @@ DlgboxCbw_err Dlgbox_DisableComboboxWideDrop(HWND hdlg);
 #include <mswin/WindowsX2.h> // chj: for SUBCLASS_FILTER_MSG0()
 #include <_MINMAX_.h>
 
-#ifdef Combobox_EnableWideDrop_DEBUG
-#undef  vaDBG      // revoke empty effect
-#else
-#define vaDBG(...) // make vaDBG empty, no debugging message
+#define CxxWindowSubclass_IMPL
+#include <mswin/CxxWindowSubclass.h>
+
+
+#ifndef Combobox_EnableWideDrop_DEBUG
+#include <CHHI_vaDBG_hide.h>
 #endif
 
 
-namespace CbxEWD
+namespace ComboboxEWD
 {
 
-// Keyword used to store the original WndProc in the window's property-list
-const TCHAR* CbxEnableWideDrop_PROP_STR = _T("Combobox_EnableWideProp_workdata");
+const TCHAR *s_sigSubclass = _T("sig_ComboboxEWD");
 
-enum { HEXMAGIC = 0x20250324 };
 enum { TimerWaitMax = 1000 , TimerInterval = 100 }; // in millisec
 
 enum { ComboItemsCheckMax = 200 };
 enum { ClbExtraWidth = 10 };
 
-struct CbxWideDrop_st
+class CbxPeeker : public CxxWindowSubclass
 {
-	UINT hexmagic;
-	WNDPROC wndproc_was;
-
-	HWND hCombobox; // the dropping down combobox
-
-	bool isTimerStarted;
-	DWORD msecTimerStart;
-
-	CbxWideDrop_st(WNDPROC wndproc_was)
+public:
+	CbxPeeker() 
 	{
-		this->hexmagic = HEXMAGIC;
-		this->wndproc_was = wndproc_was;
-
 		this->hCombobox = NULL;
 		this->isTimerStarted = false;
 		this->msecTimerStart = 0;
 	}
 
+	virtual LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+	MsgRelay_et Dlg_OnCommand(HWND hdlg, int id, HWND hwndCtl, UINT codeNotify);
+
+public:
+	HWND hCombobox; // the dropping down combobox
+
+	bool isTimerStarted;
+	DWORD msecTimerStart;
 };
+
 
 struct EnumChildWnd_st {
 	int idx;
@@ -102,7 +103,7 @@ static BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM ctx)
 		DWORD ws = GetWindowStyle(hwnd);
 		bool isVisible = ws & WS_VISIBLE ? true : false;
 
-		vaDBG(_T("  [ComboLBox #%d](%s) 0x%X, X=%d,Y=%d , w=%d,h=%d (pid=%d)%s"),
+		vaDBG2(_T("  [ComboLBox #%d](%s) 0x%X, X=%d,Y=%d , w=%d,h=%d (pid=%d)%s"),
 			ecw.idx+1, 
 			isVisible ? _T("visi") : _T("hide"),
 			hwnd,
@@ -125,7 +126,7 @@ static BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM ctx)
 static void CALLBACK TimerProc_ProbeComboLBox(
 	HWND hdlg, UINT uMsg, UINT_PTR idEvent, DWORD dwTime);
 
-static void worki_StartTimer(HWND hdlg, HWND hcbx, CbxWideDrop_st &worki)
+static void worki_StartTimer(HWND hdlg, HWND hcbx, CbxPeeker &worki)
 {
 	worki.hCombobox = hcbx;
 	worki.isTimerStarted = true;
@@ -134,7 +135,7 @@ static void worki_StartTimer(HWND hdlg, HWND hcbx, CbxWideDrop_st &worki)
 	SetTimer(hdlg, (UINT_PTR)&worki, TimerInterval, TimerProc_ProbeComboLBox);
 }
 
-static void worki_StopTimer(HWND hdlg, CbxWideDrop_st &worki)
+static void worki_StopTimer(HWND hdlg, CbxPeeker &worki)
 {
 	worki.hCombobox = NULL;
 	worki.isTimerStarted = false;
@@ -177,7 +178,7 @@ void do_AdjustClbWidth(HWND hcbx, HWND hComboLBox, int clb_old_width)
 		SIZE size = {};
 		GetTextExtentPoint32(hdc, tbuf, tlen, &size);
 
-		vaDBG(_T("  [Combobox item #%d] pixel width: %d"), i, size.cx);
+		vaDBG2(_T("  [Combobox item #%d] pixel width: %d"), i, size.cx);
 
 		clb_new_width = _MAX_(clb_new_width, size.cx + scrollbar_width + ClbExtraWidth);
 	}
@@ -185,7 +186,7 @@ void do_AdjustClbWidth(HWND hcbx, HWND hComboLBox, int clb_old_width)
 	SelectFont(hdc, hfontOld);
 	ReleaseDC(hComboLBox, hdc);
 
-	vaDBG(_T("  ComboLBox text width max pixels: %d"), clb_new_width);
+	vaDBG2(_T("  ComboLBox text width max pixels: %d"), clb_new_width);
 
 	// Now actually change ComboLBox window size
 	RECT rc = {};
@@ -197,7 +198,7 @@ void do_AdjustClbWidth(HWND hcbx, HWND hComboLBox, int clb_old_width)
 
 void CALLBACK TimerProc_ProbeComboLBox(HWND hdlg, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
-	CbxWideDrop_st &worki = *(CbxWideDrop_st*)idEvent;
+	CbxPeeker &worki = *(CbxPeeker*)idEvent;
 
 	// Enum all top-level windows to find a visible "ComboLBox"
 
@@ -210,7 +211,7 @@ void CALLBACK TimerProc_ProbeComboLBox(HWND hdlg, UINT uMsg, UINT_PTR idEvent, D
 	{
 		int clb_width = ecw.rcComboLBox.right-ecw.rcComboLBox.left;
 
-		vaDBG(_T("TimerProc: Found a visible ComboLBox window (hwnd=0x%08X), width=%dpx"), 
+		vaDBG2(_T("TimerProc: Found a visible ComboLBox window (hwnd=0x%08X), width=%dpx"), 
 			hClb, clb_width);
 		
 		do_AdjustClbWidth(worki.hCombobox, hClb, clb_width);
@@ -224,7 +225,7 @@ void CALLBACK TimerProc_ProbeComboLBox(HWND hdlg, UINT uMsg, UINT_PTR idEvent, D
 
 		if( int(msecNow - msecEnd) >= 0 )
 		{
-			vaDBG(_T("TimerProc: Not seeing a visible ComboLBox window after timeout(%d millisec)"), TimerWaitMax);
+			vaDBG2(_T("TimerProc: Not seeing a visible ComboLBox window after timeout(%d millisec)"), TimerWaitMax);
 		
 			worki_StopTimer(hdlg, worki);
 		}
@@ -232,17 +233,16 @@ void CALLBACK TimerProc_ProbeComboLBox(HWND hdlg, UINT uMsg, UINT_PTR idEvent, D
 }
 
 
-MsgRelay_et Dlg_OnCommand(HWND hdlg, int id, HWND hwndCtl, UINT codeNotify)
+MsgRelay_et 
+CbxPeeker::Dlg_OnCommand(HWND hdlg, int id, HWND hwndCtl, UINT codeNotify)
 {
-	CbxWideDrop_st &worki = *(CbxWideDrop_st*)GetProp(hdlg, CbxEnableWideDrop_PROP_STR);
-
 	TCHAR szClassname[80] = {};
 	GetClassName(hwndCtl, szClassname, ARRAYSIZE(szClassname));
 
 	if(_tcscmp(szClassname, _T("ComboBox"))==0)
 	{
 		HWND hcbx = hwndCtl;
-		vaDBG(_T("Combobox 0x%X (id=%d) codeNotify=%s"), 
+		vaDBG2(_T("Combobox 0x%X (id=%d) codeNotify=%s"), 
 			hcbx, id, ITCSv(codeNotify, itc::CBN_xxx_ComboBox)
 			); (void)hcbx;
 	}
@@ -251,24 +251,19 @@ MsgRelay_et Dlg_OnCommand(HWND hdlg, int id, HWND hwndCtl, UINT codeNotify)
 	{
 		HWND hcbx = hwndCtl;
 
-		if(! worki.isTimerStarted)
+		if(! isTimerStarted)
 		{
-			vaDBG(_T("Got CBN_DROPDOWN, so start a timer to wait for ComboLBox's emerge."));
-			worki_StartTimer(hdlg, hcbx, worki);
+			vaDBG2(_T("Got CBN_DROPDOWN, so start a timer to wait for ComboLBox's emerge."));
+			worki_StartTimer(hdlg, hcbx, *this);
 		}
 	}
 
 	return Relay_yes;
 }
 
-static LRESULT CALLBACK 
-Dlg_WndProc_EnableCbxWideDrop(HWND hdlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT  
+CbxPeeker::WndProc(HWND hdlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	CbxWideDrop_st *myprop = (CbxWideDrop_st*)GetProp(hdlg, CbxEnableWideDrop_PROP_STR);
-	WNDPROC oldWndProc = myprop->wndproc_was;
-
-//	logmsg(_T("Combobox uMsg: %s"), ITCSv(uMsg, WM_xxx));
-
 	switch(uMsg)
 	{
 	SUBCLASS_FILTER_MSG0(hdlg, WM_COMMAND, Dlg_OnCommand);
@@ -278,61 +273,46 @@ Dlg_WndProc_EnableCbxWideDrop(HWND hdlg, UINT uMsg, WPARAM wParam, LPARAM lParam
 		break;
 	}
 
-	// Call the original WndProc for default processing
-	return CallWindowProc(oldWndProc, hdlg, uMsg, wParam, lParam);
+	// Call into existing WndProc chain.
+	return DefSubclassProc(hdlg, uMsg, wParam, lParam);
 }
+
 
 DlgboxCbw_err _Dlgbox_EnableComboboxWideDrop(HWND hdlg)
 {
-	CbxWideDrop_st *myprop = (CbxWideDrop_st*)GetProp(hdlg, CbxEnableWideDrop_PROP_STR);
-	if(myprop)
+	if(!IsWindow(hdlg))
+		return DlgboxCbw_BadParam;
+
+	CxxWindowSubclass::ReCode_et err = CxxWindowSubclass::E_Fail;
+	CbxPeeker *peeker = CxxWindowSubclass::FetchCxxobjFromHwnd<CbxPeeker>(
+		hdlg, s_sigSubclass, 
+		TRUE, 
+		&err);
+
+	if(err==CxxWindowSubclass::E_Existed)
 		return DlgboxCbw_AlreadyEnabled;
 
-	myprop = new CbxWideDrop_st(nullptr);
-	if(!myprop)
-		return DlgboxCbw_NoMem;
+	if(!peeker)
+		return DlgboxCbw_Unknown;
 
-	WNDPROC oldWndProc = SubclassWindow(hdlg, Dlg_WndProc_EnableCbxWideDrop);
-	myprop->wndproc_was = oldWndProc;
-
-	BOOL succ = SetProp(hdlg, CbxEnableWideDrop_PROP_STR, myprop); // Store custom props into hcbx.
-	if(succ)
-	{
-		return DlgboxCbw_Succ;
-	}
-	else
-	{	// restore old state
-		SubclassWindow(hdlg, oldWndProc);
-		delete myprop;
-		return DlgboxCbw_SetProp;
-	}
+	return DlgboxCbw_Succ;
 }
 
 DlgboxCbw_err _Dlgbox_DisableComboboxWideDrop(HWND hdlg)
 {
-	WNDPROC currentWndProc = (WNDPROC)GetWindowLongPtr(hdlg, GWLP_WNDPROC);
-	CbxWideDrop_st *myprop = (CbxWideDrop_st*)GetProp(hdlg, CbxEnableWideDrop_PROP_STR);
+	if(!IsWindow(hdlg))
+		return DlgboxCbw_BadParam;
 
-	if(!myprop)
+	CxxWindowSubclass::ReCode_et err = CxxWindowSubclass::E_Fail;
+	CbxPeeker *peeker = CxxWindowSubclass::FetchCxxobjFromHwnd<CbxPeeker>(
+		hdlg, s_sigSubclass, 
+		FALSE, // only fetch existing
+		&err);
+
+	if(err==CxxWindowSubclass::E_NotExist)
 		return DlgboxCbw_NotEnabledYet;
 
-	if(currentWndProc != Dlg_WndProc_EnableCbxWideDrop)
-	{
-		// Someone else has further subclassed this hEdit, so we should not unsubclass it.
-		return 	DlgboxCbw_ChainMoved;
-	}
-
-	assert(myprop->hexmagic==HEXMAGIC);
-	assert(myprop->wndproc_was);
-
-	// Restore original WndProc for combobox
-	SubclassWindow(hdlg, myprop->wndproc_was);
-
-	// Remove our custom prop
-	RemoveProp(hdlg, CbxEnableWideDrop_PROP_STR);
-
-	// Destroy our custom prop storage
-	delete myprop;
+	peeker->DetachHwnd(true);
 
 	return DlgboxCbw_Succ;
 }
@@ -344,15 +324,18 @@ DlgboxCbw_err _Dlgbox_DisableComboboxWideDrop(HWND hdlg)
 //
 DlgboxCbw_err Dlgbox_EnableComboboxWideDrop(HWND hdlg)
 {
-	return CbxEWD::_Dlgbox_EnableComboboxWideDrop(hdlg);
+	return ComboboxEWD::_Dlgbox_EnableComboboxWideDrop(hdlg);
 }
 DlgboxCbw_err Dlgbox_DisableComboboxWideDrop(HWND hdlg)
 {
-	return CbxEWD::_Dlgbox_DisableComboboxWideDrop(hdlg);
+	return ComboboxEWD::_Dlgbox_DisableComboboxWideDrop(hdlg);
 }
 
 
+#ifndef Combobox_EnableWideDrop_DEBUG
+#include <CHHI_vaDBG_show.h>
+#endif
 
 #endif // Combobox_EnableWideDrop_IMPL
 
-#endif // __Combobox_EnableWideDrop_h_
+#endif
