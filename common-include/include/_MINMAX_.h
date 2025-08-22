a1ce9bc2d6e6fb0080730b48daddcd643847e1e1
chj <chjfth@gmail.com>
2025-08-22 16:23:49 +0800

    make _MIN_, _MAX_ C++ template.

--- common-include/include/_MINMAX_.h
+++ common-include/include/_MINMAX_.h
@@ -1,9 +1,15 @@
-#ifndef ___MINMAX__H
-#define ___MINMAX__H
+#ifndef ___MINMAX__H_20250822_
+#define ___MINMAX__H_20250822_
 
-#define _MIN_(a,b) ((a)<=(b) ? (a) : (b))
-#define _MAX_(a,b) ((a)>=(b) ? (a) : (b))
 
-#define _MID_(a,b,c) ( _MAX_(a,_MIN_(b,c)) )
+template<typename T> T _MIN_(T x, T y) { return x < y ? x : y; }
+template<typename T> T _MAX_(T x, T y) { return x > y ? x : y; }
+
+template<typename T> T _MID_(T x, T m, T y) 
+{ 
+	// Pull m into the range of [x,y] .
+	return _MAX_(x, _MIN_(m, y));
+}
+
 
 #endif
