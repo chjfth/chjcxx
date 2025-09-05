c4d8e42ae6a2233595c527b906203e1ad81f4b23
chj <chjfth@gmail.com>
2025-09-05 10:34:16 +0800

    Add function: parse_cmdparam_HEXRR()

--- common-include/include/mswin/utils_env.h
+++ common-include/include/mswin/utils_env.h
@@ -22,6 +22,13 @@ const TCHAR* GetExeFilename();
 
 const TCHAR* env_GetCpuArch();
 
+TCHAR *parse_cmdparam_HEXRR(
+	const TCHAR *T_cmdline, TCHAR outbuf[], int outbuflen, 
+	int *p_retlen=nullptr, // optional
+	bool *p_prefer_hexinput=nullptr // optional
+	); // // return just outbuf.
+	// note: was named parse_cmdparam_TCHARs() from G:\gitw\bookcode-mswin\share\vaDbg.cpp
+
 
 
 ///////////////////////////////////////////////////////////////
@@ -120,6 +127,125 @@ const TCHAR* env_GetCpuArch()
 	return _T("CpuArch_Unknown");
 #endif
 }
+
+
+#include <ShellAPI.h> // CommandLineToArgvW
+
+TCHAR *parse_cmdparam_HEXRR(
+	const TCHAR *T_cmdline, TCHAR outbuf[], int outbuflen, 
+	int *p_retlen, bool *p_prefer_hexinput)
+{
+/*  T_cmdline should points to string from GetCommandLineA()/GetCommandLineW().
+
+	If TCHAR is char (ANSI version), outbuf[] will be a char array.
+	If TCHAR is WCHAR (Unicode version), outbuf[] will be a WCHAR array.
+
+	Actual output TCHAR count will be returned in *p_retlen.
+
+	If user passes more than one parameter, like this:
+
+		EXENAME 41 42 7535 43 44
+
+	Then each parameter will be considered a TCHAR in HEXRR representation, 
+	and user gets 5 TCHARs on return.
+
+	So in ANSI version, user will get sth. equivalent to:
+
+		const char *outbuf = "\x41\x42\x35\x43\x44";
+
+	In Unicode version, user will get sth. equivalent to:
+		
+		const WCHAR *outbuf = "\x0041\x0042\x7535\x0043\x0044";
+
+	If only one parameter is given, like this:
+
+		EXENAME "AB cde"
+
+	The string of "AB cde" (6 TCHARs) will be returned.
+
+	p_prefer_hexinput: [in/out]
+		* As input, this matters only when T_cmdline has an single param, e.g.:
+			
+			EXENAME AB
+		  
+		  - If *p_prefer_hexinput==false, "AB" is considered a literal string,
+		    so outbuf[] will contain two TCHARs "AB".
+		  - If *p_prefer_hexinput==true, "AB" is considered a single TCHAR represented
+		    in HEXRR of value 0xAB, so the outbuf[] will contain a single TCHAR.
+
+		* As output, tells whether T_cmdline is considered literal or hex by the API.
+
+		I think in most user test-program scenario, user don't need to care this
+		p_prefer_hexinput and just pass nullptr for it.
+
+*/
+	//const TCHAR *T_cmdline = GetCommandLine();
+	const WCHAR *W_cmdline = nullptr;
+
+	if(outbuflen<=0)
+		return nullptr;
+
+	if(p_retlen)
+		*p_retlen = 0;
+
+	bool prefer_hexinput = false;
+	if(p_prefer_hexinput)
+	{
+		prefer_hexinput = *p_prefer_hexinput;
+		*p_prefer_hexinput = false;
+	}
+
+#ifdef UNICODE
+	W_cmdline = T_cmdline;
+#else
+	WCHAR W_buf[1024] = {};
+	W_cmdline = W_buf;
+	MultiByteToWideChar(CP_ACP, 0, T_cmdline, -1, W_buf, ARRAYSIZE(W_buf));		
+#endif
+
+	int argc = 0;
+	WCHAR **argv = CommandLineToArgvW(W_cmdline, &argc);
+	// -- argv[0] is exepath itself.
+
+	if(argc==2 && !prefer_hexinput)
+	{
+		// not hexform input:
+#ifdef UNICODE
+		_tcscpy_s(outbuf, outbuflen, argv[1]);
+		int retlen = (int)wcslen(argv[1]);
+#else
+		int retlen = WideCharToMultiByte(CP_ACP, 0, argv[1], -1, outbuf, outbuflen, NULL, NULL);
+#endif
+		if(p_retlen)
+			*p_retlen = retlen;
+
+		LocalFree(argv);
+		return outbuf;
+	}
+
+	// hexform input: (including argc==1 case)
+
+	int cycles = min(outbuflen-1, argc-1);
+
+	int i;
+	for(i=1; i<=cycles; i++)
+	{
+		outbuf[i-1] = (TCHAR)wcstoul(argv[i], nullptr, 16);
+	}
+
+	// To make it caller friendly, we always append a NUL char, but don't count it.
+	outbuf[cycles] = '\0';
+
+	if(p_retlen)
+		*p_retlen = cycles;
+
+	if(p_prefer_hexinput)
+		*p_prefer_hexinput = true;
+
+	LocalFree(argv);
+	return outbuf;
+}
+
 
 
 #endif // utils_env_IMPL
