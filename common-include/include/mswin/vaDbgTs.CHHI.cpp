d33a5f8fd27409d9463dd968a3e6ef6852db5a97
chj Win7Evernote VM <chjfth@gmail.com>
2025-11-14 11:36:33 +0800

    Improve vaDbgTs: Automatically check system clock bias and adjust dbg timestamp to math current system walltime.

--- common-include/include/mswin/vaDbgTs.CHHI.cpp
+++ common-include/include/mswin/vaDbgTs.CHHI.cpp
@@ -9,21 +9,18 @@
 #include <stdlib.h>
 #include <wchar.h>
 
+#include <commdefs.h>
+
 //////////////////////////////// #include done ////////////////////////////////
 
 #ifndef WinMultiMon_DEBUG
 #include <CHHI_vaDBG_hide.h>
 #endif
 
-namespace ns_vaDbgTs { // to protect our private symbols
+namespace CHHI_vaDbgTs { // private namespace
 
-const int DBG_BUFCHARS = 16000;
 
-static int s_dbgcount = 0;
-
-bool s_needymd = true;
-
-inline unsigned __int64 get_qpf()
+inline Uint64 get_qpf()
 {
 	LARGE_INTEGER li = {};
 	BOOL succ = QueryPerformanceFrequency(&li);
@@ -33,7 +30,7 @@ inline unsigned __int64 get_qpf()
 		return li.QuadPart;
 }
 
-inline unsigned __int64 get_qpc()
+inline Uint64 get_qpc()
 {
 	LARGE_INTEGER li = {};
 	BOOL succ = QueryPerformanceCounter(&li);
@@ -43,61 +40,252 @@ inline unsigned __int64 get_qpc()
 		return li.QuadPart;
 }
 
-} // namespace ns_vaDbgTs
-
-using namespace ns_vaDbgTs;
-
-bool vaDbgTs_NeedYmd(bool ymd)
-{
-	int oldval = s_needymd;
-	s_needymd = ymd;
-	return oldval;
-}
-
 unsigned int va_millisec()
 {
-	static __int64 s_qpf = get_qpf();
+	// In vaDbgTs libcode, we'll use this function an more accurate GetTickCount().
+	// 32bit is enough bcz user should care for the diff of two va_millisec() calls.
+
+	Uint64 s_qpf = get_qpf(); // To play it safe, not `static`
 
 	// We should use QueryPerformanceCounter(), 
-	// bcz GetTickCount() only has 15.6 ms resolution.
+	// bcz GetTickCount() only has 15.625 ms resolution.
 	DWORD millisec = DWORD(get_qpc()*1000 / s_qpf);
 	return millisec;
 }
 
-TCHAR* now_timestr(TCHAR buf[], int bufchars)
+const Uint64 msec_epoch_diff = (11644473600ULL * 1000); // diff in Windows-Unix epoch
+
+Uint64 SystimeToUemsec()
 {
-	SYSTEMTIME st = { 0 };
+	SYSTEMTIME st = {};
+	FILETIME ft = {};
+	GetSystemTime(&st);
+
+	SystemTimeToFileTime(&st, &ft);
+	ULARGE_INTEGER ft64;
+	ft64.HighPart = ft.dwHighDateTime;
+	ft64.LowPart = ft.dwLowDateTime;
+
+	Uint64 ret = ft64.QuadPart / 10000 - msec_epoch_diff; // result in millisec
+	return ret;
+}
+
+SYSTEMTIME UemsecToLocalTime(Uint64 uemsec)
+{
+	LARGE_INTEGER ft64;
+	ft64.QuadPart = (uemsec + msec_epoch_diff) * 10000; // millisec to 100ns
+
+	FILETIME ftUtc = {}, ftLocal = {};
+	ftUtc.dwHighDateTime = ft64.HighPart;
+	ftUtc.dwLowDateTime = ft64.LowPart;
+
+	FileTimeToLocalFileTime(&ftUtc, &ftLocal);
+
+	SYSTEMTIME stLocal = {};
+	FileTimeToSystemTime(&ftLocal, &stLocal);
+	return stLocal;
+}
+
+void ivlDbgTs(const TCHAR *fmt, va_list args) // internal debug interface
+{
+	TCHAR buf[200];
+
+	SYSTEMTIME st = {};
 	GetLocalTime(&st);
+
+	_sntprintf_s(buf, _TRUNCATE, _T("{%04d-%02d-%02d_%02d:%02d:%02d}ivaDbg: "),
+		st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+
+	OutputDebugString(buf);
+
+	_vsntprintf_s(buf, _TRUNCATE, fmt, args);
+
+	OutputDebugString(buf);
+	OutputDebugString(_T("\n"));
+}
+
+void ivaDbgTs1(const TCHAR *fmt, ...) // internal debug interface
+{
+#ifdef vaDbgTs_DEBUG1
+	va_list args;
+	va_start(args, fmt);
+	ivlDbgTs(fmt, args);
+	va_end(args);
+#else
+	(void)fmt;
+#endif
+}
+
+void ivaDbgTs2(const TCHAR *fmt, ...) // internal debug interface
+{
+#ifdef vaDbgTs_DEBUG2
+	va_list args;
+	va_start(args, fmt);
+	ivlDbgTs(fmt, args);
+	va_end(args);
+#else
+	(void)fmt;
+#endif
+}
+
+void default_output_proc(const TCHAR *dbgstr, void *ctx)
+{
+	(void)ctx;
+	OutputDebugString(dbgstr);
+}
+
+
+const int msec_bias_tolerant = 1000;
+
+int g_msec_check_interval = 10000;
+
+const int DBG_BUFCHARS = 16000;
+
+static int g_dbgcount = 0;
+
+unsigned int g_opts = vaDbg_all;
+
+PROC_vaDbg_output *g_vadbg_output = default_output_proc;
+void *g_ctx_output = 0;
+
+
+void vaDbg_set_output(PROC_vaDbg_output proc, void *ctx)
+{
+	g_vadbg_output = proc;
+	g_ctx_output = ctx;
+}
+
+TCHAR* SystimeToString(const SYSTEMTIME &st, TCHAR buf[], int bufchars)
+{
 	buf[0] = '['; buf[1] = '\0';
 
-	if (ns_vaDbgTs::s_needymd) 
+	if (g_opts & vaDbg_ymd)
 	{
 		_sntprintf_s(buf, bufchars, _TRUNCATE, _T("%s%04d-%02d-%02d_"), buf,
 			st.wYear, st.wMonth, st.wDay);
 	}
 
-	_sntprintf_s(buf, bufchars, _TRUNCATE, _T("%s%02d:%02d:%02d]"), buf,
+	_sntprintf_s(buf, bufchars, _TRUNCATE, _T("%s%02d:%02d:%02d"), buf,
 		st.wHour, st.wMinute, st.wSecond);
+
+	if (g_opts & vaDbg_millisec)
+	{
+		_sntprintf_s(buf, bufchars, _TRUNCATE, _T("%s.%03d"), buf,
+			st.wMilliseconds);
+	}
+
+	_sntprintf_s(buf, bufchars, _TRUNCATE, _T("%s]"), buf);
 
 	return buf;
 }
 
 
-
-void vaDbgTs(const TCHAR *fmt, ...)
+struct SAccuMillisec
 {
-	va_list args;
-	va_start(args, fmt);
-	vlDbgTs(fmt, args);
-	va_end(args);
+	// nomenclature:
+	// uemsec: millisec since unix-epoch(1970-01-01), imply a static value
+	// mt: millisec ticks reported by va_millisec().
+
+	Uint64 uemsec_base;
+	Uint mt_base;
+
+	Uint mt_last_check; // previous check time for clock bias
+
+	SAccuMillisec()	{ Reset(); }
+
+	void Reset()
+	{
+		uemsec_base = SystimeToUemsec();
+		mt_base = va_millisec();
+
+		mt_last_check = mt_base;
+
+		ivaDbgTs1(_T("Reset() done. uemsec_base=%I64u, mt_base=%u"), uemsec_base, mt_base);
+	}
+
+	TCHAR* NowTimeStr(TCHAR buf[], int bufchars)
+	{
+		// This function deals with system clock bias overtime. Cases are
+		// * User changes system clock suddenly.
+		// * A VM paused, waited 1 hour then resumed, would cause QPC value diverges
+		//   from system clock. See mynote [20250906.c1].
+		// * Current va_millisec() overflows and wraps to 0.
+
+		bool is_reset = false;
+		Uint mt_now = va_millisec();
+		Uint64 uemsec_now = uemsec_base + (mt_now - mt_base);
+
+		int check_delayed = mt_now - mt_last_check;
+
+//		ivaDbgTs2(_T("==== check_delayed=%d , ckin=%d"), check_delayed, g_msec_check_interval);
+
+		if (abs(check_delayed)>=g_msec_check_interval)
+		{
+			// Check for sysclock bias
+
+			Uint64 uemsec_by_systime = SystimeToUemsec();
+
+			int uemsec_diff = int(uemsec_now - uemsec_by_systime);
+			if (abs(uemsec_diff) >= msec_bias_tolerant)
+			{
+				ivaDbgTs2(_T("check close bias, result: %d (abs>%d), need Reset()"), uemsec_diff, msec_bias_tolerant);
+
+				is_reset = true;
+				Reset();
+			}
+			else
+			{
+				ivaDbgTs2(_T("check close bias, result: %d (abs<%d), still ok"), uemsec_diff, msec_bias_tolerant);
+			}
+
+			mt_last_check = mt_now;
+		}
+
+		if(is_reset)
+		{ 
+			// In case Reset() may delay some time, we'd better refresh mt_now.
+			mt_now = mt_base;
+			uemsec_now = uemsec_base;
+		}
+
+		// Generate YMD_HMS.millisec via uemsec_now
+
+		SYSTEMTIME st = UemsecToLocalTime(uemsec_now);
+
+		buf[0] = '['; buf[1] = '\0';
+				
+		return SystimeToString(st, buf, bufchars);
+	}
+};
+
+SAccuMillisec g_accums;
+
+unsigned int vaDbgTs_options(unsigned int opts)
+{
+	Uint oldval = g_opts;
+	g_opts = opts;
+	return oldval;
 }
+
+int vaDbgTs_bias_check_interval(int seconds)
+{
+	int oldval = g_msec_check_interval / 1000;
+	
+	if(seconds>0)
+	{ 
+		g_msec_check_interval = seconds * 1000;
+	}
+
+	return oldval;
+}
+
+
 
 void vlDbgTs(const TCHAR *fmt, va_list args)
 {
 	// Note: Each calling outputs one line, with timestamp prefix.
 	// A '\n' will be added automatically at end.
 
-	static int count = 0;
 	static DWORD s_prev_msec = va_millisec();
 
 	DWORD now_msec = va_millisec();
@@ -108,41 +296,40 @@ void vlDbgTs(const TCHAR *fmt, va_list args)
 	DWORD delta_msec = now_msec - s_prev_msec;
 	if(delta_msec>=1000)
 	{
-		OutputDebugString(_T(".\n"));
+		g_vadbg_output(_T(".\n"), g_ctx_output);
 	}
 
 	TCHAR timebuf[40] = {};
-	now_timestr(timebuf, ARRAYSIZE(timebuf));
+	g_accums.NowTimeStr(timebuf, ARRAYSIZE(timebuf));
 
-	_sntprintf_s(buf, _TRUNCATE, _T("[%d]%s (+%3u.%03us) "), 
-		++s_dbgcount,
-		timebuf, 
-		delta_msec/1000, delta_msec%1000);
+	if(g_opts & vaDbg_diff)
+	{
+		_sntprintf_s(buf, _TRUNCATE, _T("[%d]%s (+%3u.%03us) "),
+			++g_dbgcount,
+			timebuf,
+			delta_msec / 1000, delta_msec % 1000);
+	}
 
 	int prefixlen = (int)_tcslen(buf);
 
 	_vsntprintf_s(buf+prefixlen, ARRAYSIZE(buf)-3-prefixlen, _TRUNCATE, fmt, args);
 
-	// add trailing \n
-	int slen = (int)_tcslen(buf);
-	if(slen==ARRAYSIZE(buf)-1)
-		--slen;
+	if(g_opts & vaDbg_newline)
+	{
+		// add trailing \n
+		int slen = (int)_tcslen(buf);
+		if(slen==ARRAYSIZE(buf)-1)
+			--slen;
 
-	buf[slen] = '\n';
-	buf[slen+1] = '\0';
+		buf[slen] = '\n';
+		buf[slen+1] = '\0';
+	}
 
-	OutputDebugString(buf);
+	g_vadbg_output(buf, g_ctx_output);
 
 	s_prev_msec = now_msec;
 }
 
-void vaDbgS(const TCHAR *fmt, ...)
-{
-	va_list args;
-	va_start(args, fmt);
-	vlDbgS(fmt, args);
-	va_end(args);
-}
 
 void vlDbgS(const TCHAR *fmt, va_list args)
 {
@@ -150,24 +337,80 @@ void vlDbgS(const TCHAR *fmt, va_list args)
 
 	TCHAR buf[DBG_BUFCHARS] = {0};
 
-	_sntprintf_s(buf, ARRAYSIZE(buf)-3, _TRUNCATE, TEXT("[%d] "), ++s_dbgcount); // prefix seq
+	_sntprintf_s(buf, ARRAYSIZE(buf)-3, _TRUNCATE, TEXT("[%d] "), ++g_dbgcount); // prefix seq
 
 	int prefixlen = (int)_tcslen(buf);
 
 	_vsntprintf_s(buf+prefixlen, ARRAYSIZE(buf)-3-prefixlen, _TRUNCATE, fmt, args);
 
-	// add trailing \n
-	int slen = (int)_tcslen(buf);
-	if(slen==ARRAYSIZE(buf)-1)
-		--slen;
+	if(g_opts & vaDbg_newline)
+	{
+		// add trailing \n
+		int slen = (int)_tcslen(buf);
+		if(slen==ARRAYSIZE(buf)-1)
+			--slen;
 
-	buf[slen] = '\n';
-	buf[slen+1] = '\0';
+		buf[slen] = '\n';
+		buf[slen+1] = '\0';
 
-	OutputDebugString(buf);
+	}
+
+	g_vadbg_output(buf, g_ctx_output);
 }
 
 
+} // private namespace
+
+////////////////////////////////////////////////////////////////////////////////// 
+//////////////////////////////// public API below ////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+unsigned int va_millisec()
+{
+	return CHHI_vaDbgTs::va_millisec();
+}
+
+void vaDbgTs(const TCHAR *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	CHHI_vaDbgTs::vlDbgTs(fmt, args);
+	va_end(args);
+}
+
+void vlDbgTs(const TCHAR *fmt, va_list args)
+{
+	CHHI_vaDbgTs::vlDbgTs(fmt, args);
+}
+
+void vaDbgS(const TCHAR *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	CHHI_vaDbgTs::vlDbgS(fmt, args);
+	va_end(args);
+}
+
+void vlDbgS(const TCHAR *fmt, va_list args)
+{
+	CHHI_vaDbgTs::vlDbgS(fmt, args);
+}
+
+
+unsigned int vaDbgTs_options(unsigned int opts)
+{
+	return CHHI_vaDbgTs::vaDbgTs_options(opts);
+}
+
+int vaDbgTs_bias_check_interval(int seconds)
+{
+	return CHHI_vaDbgTs::vaDbgTs_bias_check_interval(seconds);
+}
+
+void vaDbg_set_output(PROC_vaDbg_output proc, void *ctx)
+{
+	CHHI_vaDbgTs::vaDbg_set_output(proc, ctx);
+}
 
 #ifndef WinMultiMon_DEBUG
 #include <CHHI_vaDBG_show.h>
--- common-include/include/vaDbgTs.h
+++ common-include/include/vaDbgTs.h
@@ -1,6 +1,29 @@
 #ifndef __vaDbgTs_h_20251113_
 #define __vaDbgTs_h_20251113_
 
+/* 
+[Purpose]
+
+	vaDbgTs() provides a debug output line with timestamp prefix that is accurate to
+	one millisecond. A sample output line like this:
+
+	[1][2025-11-14_10:48:32.644] (+  0.000s) ...
+	[2][2025-11-14_10:48:33.685] (+  1.041s) ...
+	[3][2025-11-14_10:48:34.717] (+  1.031s) ...
+
+[Implementation notes]
+	
+	On Windows GetSystemTime() and GetLocalTime() can not fulfill bcz they have only
+	a PCMildTick(15.625 millisec) accuracy. So I need the help of QueryPerformanceCounter()
+	for millisec reference.
+
+	vaDbgTs deals with system clock changing by doing a bias verifying every 10 seconds
+	(configurable via vaDbgTs_bias_verify_interval).
+
+	Of course, vaDbgTs does not launch a background thread for bias check, it is only 
+	carried out when user calls vaDbgTs().
+*/
+
 #include <stdarg.h>
 
 #include <ps_TCHAR.h> // for stramphibian TCHAR
@@ -12,15 +35,35 @@ extern"C" {
 unsigned int va_millisec(); 
 // -- This function tries to provide real 1 millisecond granularity.
 // WinNT's GetTickCount() at most times has only 15.625 millisec precision.
-
-bool vaDbgTs_NeedYmd(bool ymd);
-// -- return old value
+// This should be an ever increasing value, even after user turns system clock backward.
 
 void vaDbgTs(const TCHAR *fmt, ...); // with seq+timestamp prefix
 void vlDbgTs(const TCHAR *fmt, va_list args);
 
 void vaDbgS(const TCHAR *fmt, ...);  // with sequence-only prefix
 void vlDbgS(const TCHAR *fmt, va_list args);
+
+typedef void PROC_vaDbg_output(const TCHAR *dbgstr, void *ctx);
+
+void vaDbg_set_output(PROC_vaDbg_output proc, void *ctx);
+
+unsigned int vaDbgTs_options(
+	unsigned int opts // vaDbg_ymd etc
+); // -- return old value
+
+int vaDbgTs_bias_check_interval(int seconds);
+// -- return old value
+
+enum {
+	vaDbg_seq = 1,      // leading sequence number
+	vaDbg_ymd = 2,      // show year, month, date
+	vaDbg_millisec = 4, // show millisecond
+	vaDbg_diff = 8,     // show diff seconds to previous
+	vaDbg_newline = 16, // extra \n at tail
+
+	vaDbg_all = vaDbg_seq | vaDbg_ymd | vaDbg_millisec | vaDbg_diff | vaDbg_newline
+};
+
 
 #ifdef __cplusplus
 } // extern"C" {
